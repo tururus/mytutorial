a578f058984d38054854767dc24365a77f66adfa
Tururus <bartekop29@gmail.com>
2018-10-11 21:06:44 +0200

    Added php file

---
+++ work_in_progress.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "Hello world!<br>Work in progress";
+echo '<img src="http://indyviolins.com/img/under_construction_sign_work_computer_humor_funny_text_maintenance_wallpaper_website_web_4000x2870.jpg" width="1000" alt="Work in progress"></img>';
+
+?>
